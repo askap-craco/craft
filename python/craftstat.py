9f422d843d42839f4a3c23a10a04e816dad07b5e
Keith Bannister <keith.bannister@csiro.au>
2017-07-24 18:24:48 +0800

    Fix up craftstat.py

--- python/craftstat.py
+++ python/craftstat.py
@@ -15,7 +15,6 @@ import itertools
 from craftobs import load_beams
 from plotutil import subplots
 import matplotlib.gridspec as gridspec
-from influxdb import InfluxDBClient
 
 __author__ = "Keith Bannister <keith.bannister@csiro.au>"
 
@@ -41,6 +40,7 @@ def _main():
     parser.add_argument('--step', type=int, help='time step', default=10)
     parser.add_argument('--ntimes', type=int, help='Numerb of samples per block', default=1024)
     parser.add_argument('-O','--outfile', help='Output file for influxdb data. Inhibits live loading')
+    parser.add_argument('--show', action='store_true', help='show plots', default=False)
     parser.set_defaults(verbose=False, nxy="1,1")
     values = parser.parse_args()
     if values.verbose:
@@ -48,77 +48,87 @@ def _main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    st = CraftStatMaker(values.files, values)
     influxout = None
     client = None
     if values.outfile:
         influxout = open(values.outfile, 'w')
     else:
+        from influxdb import InfluxDBClient
         client = InfluxDBClient(host='akingest01', database='craft')
+
+    for file in values.files:
+        try:
+            get_meas(filename, client, influxout)
+        except:
+            pass
+
+    influxout.close()
         
-    fullpath  = os.path.abspath(values.files[0])
+def get_meas(filename, client, influxout, values):
+    fullpath  = os.path.abspath(filename)
     pathbits = fullpath.split('/') # hacky way of getting sbid, scanid and ant
     print len(pathbits), pathbits
     sbid = pathbits[-5]
     scanid = pathbits[-4]
     ant = pathbits[-3]
     startid = pathbits[-2]
+    filebits = filename.split('.')
+    assert filename.endswith('.fil')
+    b = int(filebits[-2])
+    st = CraftStatMaker(filename, values)
+
     while True:
         s, unix_start = st.next_stat()
         nbeams, nstat = s.shape
         # nanoseconds since unix epoch
         unix_nanosec = int(np.round(unix_start * 1e9))
-        for b in xrange(nbeams):
-            outs = 'craftstat,sbid={},scanid={},ant={},beam={} '.format(sbid, scanid, ant, b)
-            statbits = ['{}={}'.format(statname, stat) for (statname, stat) in zip(st.stat_names, s[b,: ])]
-            outs += ','.join(statbits)
-            outs += ' {}\n'.format(unix_nanosec)
-            field_dict = {}
-            for sname, v in zip(st.stat_names, s[b, :]):
-                field_dict[sname] = v
+        outs = 'craftstat,sbid={},scanid={},ant={},beam={} '.format(sbid, scanid, ant, b)
+        statbits = ['{}={}'.format(statname, stat) for (statname, stat) in zip(st.stat_names, s[b,: ])]
+        outs += ','.join(statbits)
+        outs += ' {}\n'.format(unix_nanosec)
+        field_dict = {}
+        for sname, v in zip(st.stat_names, s[b, :]):
+            field_dict[sname] = v
+            
+        body = {'measurement':'craftstat',
+                'tags':{'sbid':sbid,'scanid':scanid,'ant':ant,'beam':b},
+                'time':unix_nanosec,
+                'fields':field_dict
+                }
 
-            body = {'measurement':'craftstat',
-                    'tags':{'sbid':sbid,'scanid':scanid,'ant':ant,'beam':b},
-                    'time':unix_nanosec,
-                    'fields':field_dict
-                    }
+        if influxout is not None:
+            influxout.write(outs)
 
-            if influxout is not None:
-                influxout.write(outs)
-
-            if client is not None:
-                client.write_points([body])
+        if client is not None:
+            client.write_points([body])
 
 class CraftStatMaker(object):
-    def __init__(self, files, values):
-        self.files = files
+    def __init__(self, fname, values):
+        self.fname = fname
         self.values = values
         self.ntimes = values.ntimes
         self.tstep = values.step
         self.tstart = 0
         self.fft_freqs = np.array([38.5, 50, 100, 200, 300])
-        beams, self.spfiles = load_beams(self.files, self.tstart, self.ntimes, return_files=True)
+        self.spfile = sigproc.SigprocFile(self.fname)
+        self.mjdstart = self.spfile.tstart
+        self.tsamp = self.spfile.tsamp
         self.stat_names = ['bmean','bstd', 'dm0std']
         for f in self.fft_freqs:
             self.stat_names.append('f{}'.format(f))
 
     def next_data(self):
+        self.tstart += self.tstep + self.ntimes
+        self.spfile.seek_sample(self.tstart)
+        assert self.spfile.nbits == 8
+        d = np.fromfile(self.spfile.fin, dtype=np.uint8, count=self.ntimes*self.spfile.nchans)
+        d.shape = (self.ntimes, 1, self.spfile.nchans)
 
-        for ifin, f in enumerate(self.spfiles):
-            f.seek_sample(self.tstart)
-            nelements = f.nchans*self.ntimes
-            dtype = np.uint8
-            v = np.fromfile(f.fin, dtype=dtype, count=nelements )
-
-            
+        return  d
 
     def next_stat(self):
-        beams, files = load_beams(self.files, self.tstart, self.ntimes, return_files=True)
-        mjdstart = files[0].tstart
-        tsamp = files[0].tsamp
 
-        self.mjdstart = mjdstart
-        self.tsamp = tsamp
+        bi = self.next_data()
         mjdtime = self.mjdstart + self.tstart * self.tsamp/3600./24.
         unix_start = (self.mjdstart - MJD_UNIX_EPOCH)*86400.
         unix_time = unix_start + self.tstart * self.tsamp 
@@ -128,30 +138,27 @@ class CraftStatMaker(object):
         beams -= 128
         beams /= 18
 
-        ntimes, nbeams, nfreq = beams.shape
-        stat = np.zeros((nbeams, 3 + len(self.fft_freqs)))
+        stat = np.zeros((1, 3 + len(self.fft_freqs)))
 
-        for i in xrange(nbeams):
-            bi = beams[:, i, :]
-            ntimes, nfreq = bi.shape
+        ntimes, nfreq = bi.shape
             # spectra
-            beam_mean = bi.mean(axis=0)
-            beam_std = bi.std(axis=0)
-            bstd = beam_std.std()
-            # dm0
-            dm0 = bi.mean(axis=1)
-            dm0std = dm0.std()
+        beam_mean = bi.mean(axis=0)
+        beam_std = bi.std(axis=0)
+        bstd = beam_std.std()
+# dm0
+        dm0 = bi.mean(axis=1)
+        dm0std = dm0.std()
 
-            stat[i, 0] = beam_mean.mean()
-            stat[i, 1] = bstd
-            stat[i, 2] = dm0std
+        stat[i, 0] = beam_mean.mean()
+        stat[i, 1] = bstd
+        stat[i, 2] = dm0std
 
-            dm0f = abs(np.fft.rfft(dm0, axis=0))**2
-            ntimes, nchans = bi.shape
+        dm0f = abs(np.fft.rfft(dm0, axis=0))**2
+        ntimes, nchans = bi.shape
 
-            idxs = np.rint(self.fft_freqs * float(ntimes) * self.tsamp).astype(int)
-            stat[i, 3:] = dm0f[idxs]/dm0std
-            fftfreqs  = np.arange(len(dm0f))/float(ntimes)/self.tsamp
+        idxs = np.rint(self.fft_freqs * float(ntimes) * self.tsamp).astype(int)
+        stat[i, 3:] = dm0f[idxs]/dm0std
+        fftfreqs  = np.arange(len(dm0f))/float(ntimes)/self.tsamp
 
             #pylab.plot(fftfreqs, dm0f)
             #for f in self.fft_freqs:
@@ -159,14 +166,6 @@ class CraftStatMaker(object):
             #pylab.show()
 
         return stat, unix_time
-
-
-def getstats():
-    tstart = self.tstart
-    ntimes = self.ntimes
-    beams, files = load_beams(self.files, tstart, ntimes, return_files=True)
-
-
 
 if __name__ == '__main__':
     _main()
