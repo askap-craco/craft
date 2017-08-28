a02d2dd57a2b11d9f9e356d2c2dcfb3795a1725d
Keith Bannister <keith.bannister@csiro.au>
2017-08-28 14:13:25 +0800

    Add jobs - getting ready to move to group - bleah

---
+++ jobs/fixhdr.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=24
+
+export CRAFT=/home/ban115/craft/craft/
+export PATH=$CRAFT/cuda-fdmt/cudafdmt/src:$CRAFT/python:$PATH:/home/ban115/bin:$CRAFT/jobs/
+export PYTHONPATH=$CRAFT/python:$PYTHONPATH
+export OMP_NUM_THREADS=24
+
+module unload PrgEnv-cray
+module unload gcc/4.9.0
+module load python/2.7.10
+module load astropy
+module load pyephem
+
+
+thedir=$1
+thebase=`basename $thedir`
+# Array = co or ak
+array=co
+pset=/home/ban115/fixheaders/$array/sbpars/
+parset=$pset/${thebase}.parset
+sblist=$pset/sblist.txt
+
+echo "`date` Fixing SB in directory $thedir with base $thebase. Parset=$parset sblist=$sblist"
+
+if [[ ! -e $parset ]] ; then
+    echo Parset $parset doesnt exist
+    exit 1
+fi
+
+if [[ ! -e $sblist ]] ; then
+    echo SBLIST $sblist doesnt exist
+    exit 1
+fi
+
+find $thedir -name "${array}*.hdr" | xargs fix_headers.py --parset $parset  --sblist  $sblist --fix
