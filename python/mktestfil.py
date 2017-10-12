9a7884bf75e3c1d328f2a00299ce85dd09ddc270
Bannister, Keith (CASS, Marsfield) <Keith.Bannister@csiro.au>
2017-10-12 21:57:31 +1100

    Fix up mktestfil

--- python/mktestfil.py
+++ python/mktestfil.py
@@ -69,9 +69,37 @@ def _main():
            'nifs':1
        }
     
+    assert 0 <= values.offset <= 255
+    if values.idt is not None:
+        t_total = float(values.idt) * tsamp
+        values.dm = -t_total/(f1**-2 - f2**-2)/4.15
+        assert values.dm >= 0
+        print 'idt=', values.idt, 'dm=', values.dm
+
+    sfile = sigproc.SigprocFile(values.files[0], 'w', hdr)
+    sfile.seek_data(0)
+
+    if values.dm is None:
+        for dm in xrange(1, 2000):
+            print dm, hdr, values
+            d = mkfrb(float(dm), hdr, values)
+            print 'Writing ', d.shape, d.dtype, 'to', values.files[0]
+            d.tofile(sfile.fin)
+
+    else:
+        d = mkfrb(values.dm, hdr, values)
+        print 'Writing ', d.shape, d.dtype, 'to', values.files[0]
+        d.tofile(sfile.fin)
+        
+    sfile.fin.flush()
+    sfile.fin.close()
+
+
+
+def mkfrb(dm,  hdr, values):
     nchans = hdr['nchans']
     nifs = hdr['nifs']
-    ntimes = 16384/2
+    ntimes = 4096
     shape = (ntimes, nifs, nchans)
     f1 = hdr['fch1']/1e3
     foff = hdr['foff']/1e3
@@ -79,70 +107,61 @@ def _main():
     tsamp = hdr['tsamp']*1000.
     fstart = f1 + foff*0.5
 
-    assert 0 <= values.offset <= 255
-    d = np.random.randn(np.prod(shape))*values.noiserms + values.offset
-    d.shape = shape
-    print 'Shape', d.shape
-
     offset = values.toffset
 
-    if values.idt is not None:
-        t_total = float(values.idt) * tsamp
-        values.dm = -t_total/(f1**-2 - f2**-2)/4.15
-        assert values.dm >= 0
-        print 'idt=', values.idt, 'dm=', values.dm
+    d = np.random.randn(np.prod(shape))*values.noiserms + values.offset
+    d.shape = shape
+    t = 0    
 
-    if values.dm is not None:
-        t = 0
-        while True:
-            t1 = t * tsamp
-            t2 = (t + 1) * tsamp
-
-            c1,frqt1 = dmchanof(f1,foff, t1, values.dm)
-            c2,frqt2 = dmchanof(f1,foff, t2, values.dm)
-
-            fc1 = f1 + c1*foff
-            fc2 = f1 + c2*foff
-
-            # This logic might not be right
-            if c2 >= nchans:
-                c2 = nchans-1
+    while True:
+        t1 = t * tsamp
+        t2 = (t + 1) * tsamp
+        
+        c1,frqt1 = dmchanof(f1,foff, t1, dm)
+        c2,frqt2 = dmchanof(f1,foff, t2, dm)
+        
+        fc1 = f1 + c1*foff
+        fc2 = f1 + c2*foff
+        
+        # This logic might not be right
+        if c2 >= nchans:
+            c2 = nchans-1
             currchan = c1
 
-            print 'dm', values.dm, 't', t, c1, c2, t1, t2, frqt1, frqt2
-            if c1 < 0 or c1 >= nchans or (t + offset) >= ntimes:
-                print 'breaking', c1, nchans, (t+offset), ntimes
-                break
+        print 'dm', dm, 't', t, c1, c2, t1, t2, frqt1, frqt2
+        if c1 < 0 or c1 >= nchans or (t + offset) >= ntimes:
+            print 'breaking', c1, nchans, (t+offset), ntimes
+            break
 
-            #d[t+offset, 0, c1:c2+1] += values.pulse_amplitude
-            tin1 = dmdelay(values.dm, frqt1, fc1-foff/2.) # time spent between the pulse start freq, and bottom of the channel
-            tin11 = dmdelay(values.dm, fc1+foff/2., fc1-foff/2.) # Time we could have spent, had the pulse started in the top, rather than the side
+        #d[t+offset, 0, c1:c2+1] += values.pulse_amplitude
+        tin1 = dmdelay(dm, frqt1, fc1-foff/2.) # time spent between the pulse start freq, and bottom of the channel
+        tin11 = dmdelay(dm, fc1+foff/2., fc1-foff/2.) # Time we could have spent, had the pulse started in the top, rather than the side
 
-            tin2 = dmdelay(values.dm, fc2+foff/2., frqt2)
-            tin22 = dmdelay(values.dm, fc2+foff/2., fc2-foff/2.)
+        tin2 = dmdelay(dm, fc2+foff/2., frqt2)
+        tin22 = dmdelay(dm, fc2+foff/2., fc2-foff/2.)
 
-            print 'c1',c1,'f1',fc1,'tin1',tin1, 'tin11', tin11, tin1/tin11
-            print 'c2',c2,'f2',fc2,'tin2',tin2, 'tin22', tin22, tin2/tin22
-            assert tin1 > 0
-            assert tin2 > 0
+        print 'c1',c1,'f1',fc1,'tin1',tin1, 'tin11', tin11, tin1/tin11
+        print 'c2',c2,'f2',fc2,'tin2',tin2, 'tin22', tin22, tin2/tin22
+        assert tin1 > 0
+        assert tin2 > 0
 
-            assert c2 >= c1
-            amp = values.pulse_amplitude
-            if c1 == c2:
-                d[t+offset, 0, c1] += amp
-            else:
-                d[t+offset, 0, c1] += amp*(tin1/tin11)**1
-                d[t+offset, 0, c2] += amp*(tin2/tin22)**1
-                print 'Setting %d=%f and %d=%f'% (c1, amp*tin1/tin11, c2, amp*tin2/tin22), d[t+offset, 0, c1], d[t+offset, 0, c2]
+        assert c2 >= c1
+        amp = values.pulse_amplitude
+        if c1 == c2:
+            d[t+offset, 0, c1] += amp
+        else:
+            d[t+offset, 0, c1] += amp*(tin1/tin11)**1
+            d[t+offset, 0, c2] += amp*(tin2/tin22)**1
+            print 'Setting %d=%f and %d=%f'% (c1, amp*tin1/tin11, c2, amp*tin2/tin22), d[t+offset, 0, c1], d[t+offset, 0, c2]
 
-            if c2 - c1 > 1:
-                d[t + offset, 0, c1+1:c2] += amp
-                print 'Setting', c1+1, c2-1, 'to', amp, d[t+offset, 0, c1+1]
+        if c2 - c1 > 1:
+            d[t + offset, 0, c1+1:c2] += amp
+            print 'Setting', c1+1, c2-1, 'to', amp, d[t+offset, 0, c1+1]
 
                 
 
             #d[t+offset, 0, c1:c2+1] += values.pulse_amplitude
-            t += 1
+        t += 1
             
     if values.show:
         pylab.imshow(d)
@@ -151,13 +170,7 @@ def _main():
 
 
     d = d.astype(np.uint8)
-    sfile = sigproc.SigprocFile(values.files[0], 'w', hdr)
-    sfile.seek_data(0)
-    print 'Writing ', d.shape, d.dtype, 'to', values.files[0]
-    d.tofile(sfile.fin)
-    sfile.fin.flush()
-    sfile.fin.close()
-    
+    return d
     
 
 if __name__ == '__main__':
