630dec4cee04f2a99cb4248fdbb1c92f1571a222
Keith Bannister <keith.bannister@csiro.au>
2018-03-19 11:39:46 +0800

    Add error to archivesinglescan

--- jobs/fixhdr.sh
+++ jobs/fixhdr.sh
@@ -3,9 +3,10 @@
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=24
 
+export ACES=/home/ban115/ACES
 export CRAFT=/home/ban115/craft/craft/
 export PATH=$CRAFT/cuda-fdmt/cudafdmt/src:$CRAFT/python:$PATH:/home/ban115/bin:$CRAFT/jobs/
-export PYTHONPATH=$CRAFT/python:$PYTHONPATH
+export PYTHONPATH=$ACES/pythonlib:$CRAFT/python:$PYTHONPATH
 export OMP_NUM_THREADS=24
 
 module unload PrgEnv-cray
@@ -13,6 +14,7 @@ module unload gcc/4.9.0
 module load python/2.7.10
 module load astropy
 module load pyephem
+
 
 
 thedir=$1
@@ -36,6 +38,6 @@ if [[ ! -e $sblist ]] ; then
 fi
 
 find $thedir -name "${array}*.hdr" | xargs fix_headers.py --parset-dir $pset  --sblist  $sblist --fix
-find $thedir -name "${array}*.hdr.v2" | xargs summarise_scans.py --write-meta-files --outfile /home/ban115/archive/v8/$thebase.json
+find $thedir -name "${array}*.hdr.v2" | xargs summarise_scans.py --write-meta-files --outfile /home/ban115/archive/v9/$thebase.json
 
-echo "`date` Finished ixing SB in directory $thedir with base $thebase. Parset=$parset sblist=$sblist on host `hostname`"
+echo "`date` Finished fixing SB in directory $thedir with base $thebase. Parset=$parset sblist=$sblist on host `hostname`"
