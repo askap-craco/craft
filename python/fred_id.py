ee4c164e3b412f4804066533e2e78dfe433789ee
Harry <hqiu678@outlook.com>
2018-04-12 13:22:15 +1000

    new xmatch with astropy

--- python/fred_id.py
+++ python/fred_id.py
@@ -11,9 +11,7 @@ import numpy as np
 import os
 import sys
 import logging
-#import sigproc as sgp
 from astropy import units as u
-from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 '''
 dev:python3
@@ -93,7 +91,28 @@ def fredda_read(fredda,beamno):
     #dec = coords.dec.deg
     return bm_values,fof_array
 
-def searcher(beamno,ra,dec,blist,foflines,idio,psrname,psrdm,psrra,psrdec,rad):
+def xmatch_astropy(i,ra,dec,blist,foflines,idio,psrname,cat,rdm):
+    dm_values=blist.T[6]
+    if len(foflines)==1:
+        dm_values=[dm_values]
+    ra1=np.ones(len(dm_values))*ra
+    dec1=np.ones(len(dm_values))*dec
+    c = SkyCoord(ra=ra1*u.degree, dec=dec1*u.degree,distance=dm_values*u.dm)
+    errorbar=rdm
+    idxc, idxcatalog, d2d, d3d = catalog.search_around_3d(c, errorbar*u.dm)
+    tofrb =np.setdiff1d(np.where(c.distance.dm),idxc)
+    print (i,psrname[np.unique(idxcatalog)],len(idxcatalog))
+    for n,i in enumerate(idxc):
+        writeline=foflines[i]
+        writename=psrname[idxcatalog[n]]
+        idio.write_psr(writename+' '+writeline)
+    for i in tofrb:
+        writeline=foflines[i]
+        idio.write_frb(writeline)
+
+
+
+def searcher(ra,dec,blist,foflines,idio,psrname,psrdm,psrra,psrdec,rad,rdm):
     #searcher(i,ra,dec,dm_list,f1,f2,cat_name,cat_dm,cat_ra,cat_dec)
     #os.system("psrcat -c '"+format+"' > psrcat.csv")
     #print blist
@@ -103,7 +122,9 @@ def searcher(beamno,ra,dec,blist,foflines,idio,psrname,psrdm,psrra,psrdec,rad):
         #print 'tell'
     #select_ra=np.intersect1d(np.where(psrra>ra-1),np.where(psrra<ra+1))
     #select_dec=np.intersect1d(np.where(psrdec>dec-1),np.where(psrdec<dec+
+
     ##### query from psrcat array
+
     ### select pulsars within position
     select_pos=np.intersect1d(np.intersect1d(np.where(psrra>ra-rad),np.where(psrra<ra+rad)),np.intersect1d(np.where(psrdec>dec-rad),np.where(psrdec<dec+rad)))
     ##### extract DM information of selected pulsars
@@ -115,11 +136,12 @@ def searcher(beamno,ra,dec,blist,foflines,idio,psrname,psrdm,psrra,psrdec,rad):
     #print dm_values
     if clen>0:
         print(str(clen)+' pulsars near beam radius of '+str(rad))
+        print(psrname[select_pos])
         for i,match in enumerate(dm_values):
             #print 'number',i,match ### print line number
             writeline=foflines[i] #### transfer candidate details
             #print('candidate DM %f'%(match)) ### print dm of candidate
-            select_dm=np.intersect1d(np.where(posxdm>match-5),np.where(posxdm<match+5))
+            select_dm=np.intersect1d(np.where(posxdm>match-rdm),np.where(posxdm<match+rdm))
             if len(select_dm)==1:
                 print 'Found Candidate'
                 for j in select_dm:
@@ -131,13 +153,16 @@ def searcher(beamno,ra,dec,blist,foflines,idio,psrname,psrdm,psrra,psrdec,rad):
                         print(writename,psrdm[real_pos],"location ",ra1,dec1)
                         idio.write_psr(writename+' '+writeline)
             elif len(select_dm)>1:
+                print 'multiple'
                 r2=rad
-                while len(select_dm)>1:
+                while len(select_dm)>2:
                     r2=r2*0.75
+                    print (psrname[select_dm])
+                    print "search radius",r2
                     select_pos=np.intersect1d(np.intersect1d(np.where(psrra>ra-r2),np.where(psrra<ra+r2)),np.intersect1d(np.where(psrdec>dec-r2),np.where(psrdec<dec+r2)))
                     posxdm=psrdm[select_pos]
-                    select_dm=np.intersect1d(np.where(posxdm>match-5),np.where(posxdm<match+5))
-                if len(select_dm)==1:
+                    select_dm=np.intersect1d(np.where(posxdm>match-rdm),np.where(posxdm<match+rdm))
+                if len(select_dm)<=2:
                     print 'Found Candidate'
                     for j in select_dm:
                         real_pos=select_pos[j]
@@ -179,7 +204,7 @@ parser.add_argument('--cat',type=str,default='',help='psrcat.csv address leave e
 #parser.add_argument('-c','--column',type=str,default='name p0 dm raj decj',help='')
 parser.add_argument('-r','--radius',type=float,default=2,help='search radius')
 parser.add_argument('-x','--sncut',type=float,default=10,help='fredda.frb file threshold for sn')
-parser.add_argument('--dmcut',type=float,default=0,help='fredda.frb file threshold for dm')
+parser.add_argument('-d','--dmlim',type=float,default=5,help='dm')
 parser.add_argument(dest='files', nargs='+') ####hdr file name here
 parser.set_defaults(verbose=False)
 values = parser.parse_args()
@@ -194,6 +219,7 @@ if len(psrcat)==0:
     psrcat=os.environ['FRED_CAT']
 hdr_loc=''
 fof_loc=''
+dmlim=values.dmlim
 ############ Generating psrcat database
 if os.path.exists(psrcat):
     print("reading data from psrcat")
@@ -222,6 +248,7 @@ cat_dec=cat.T[4].astype(float)
 changedm=np.where(cat_dm=='*')
 cat_dm[changedm]=0
 cat_dm=cat_dm.astype(float)
+catalog=SkyCoord(ra=cat_ra*u.degree, dec=cat_dec*u.degree, distance=cat_dm*u.dm)
 ######################## t load header filelist and fof files
 fname=fof_loc+values.candlist
 pos=read_fredda_hdr(hdr_loc+values.files[0],46,47)
@@ -237,13 +264,17 @@ idio.open_files(fname,fname)
 #f1=open(fname+".psr",'w')
 #f2=open(fname+".frb",'w')
 for i,xy in enumerate(pos,0):
-    print(i,xy)
+    #print(i,xy)
     bm_list,fline_list=fredda_read(fredda,i)
     ra=xy[0]
     dec=xy[1]
-    print("\n ------------------- \nSearching "+str(len(fline_list))+" Candidates in Beam"+str(i))
+    #print("\n ------------------- \nSearching "+str(len(fline_list))+" Candidates in Beam"+str(i))
+    #bpos=SkyCoord(ra*u.deg,dec*u.deg)
+    #idx, d2d, d3d = bpos.match_to_catalog_sky(catalog)
+    #print cat_name[idx]
     if len(bm_list) > 0:
-        searcher(i,ra,dec,bm_list,foflines[fline_list],idio,cat_name,cat_dm,cat_ra,cat_dec,radius)
+        xmatch_astropy(i,ra,dec,bm_list,foflines[fline_list],idio,cat_name,catalog,dmlim)
+
 
 #f1.close()
 #f2.close()
