b55c73e6bcda0644c033cb9cc0718f3bd98e233b
Keith Bannister <keith.bannister@csiro.au>
2018-08-17 14:45:54 +0800

    Fix checkvcraft and tidy up for smaller expected number of samples CRAFT-192

--- python/checkvcraft.py
+++ python/checkvcraft.py
@@ -12,6 +12,7 @@ import os
 import sys
 import logging
 import vcraft
+import scipy.signal
 
 
 __author__ = "Keith Bannister <keith.bannister@csiro.au>"
@@ -21,6 +22,7 @@ def _main():
     parser = ArgumentParser(description='Check for sample delays in vcraft files', formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
     parser.add_argument('-s','--show', action='store_true', help="plot where it's all gone wrong")
+    parser.add_argument('-n','--nsamp', type=int, help='Number of samples to compare', default=4096)
     parser.add_argument(dest='files', nargs='+')
     parser.set_defaults(verbose=False)
     values = parser.parse_args()
@@ -29,12 +31,14 @@ def _main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    N = 4096*4
+    N = values.nsamp
     files = [vcraft.VcraftFile(f) for f in values.files]
     trigger_frameids = [f.trigger_frameid for f in files]
     fstart = max(trigger_frameids)
     f0 = fstart - trigger_frameids[0]
     d0 = files[0].read(f0, N)
+    chan = 4
+    ncrap = 1440
     for f in files:
         foff = fstart - f.trigger_frameid
         d = f.read(foff, N)
@@ -42,17 +46,34 @@ def _main():
 
         print '{} TRIGGER_FRAMEID={} FRAME_OFF={} equal ? {}'.format(f.fname, f.trigger_frameid, foff, is_equal)
         if not is_equal and values.show:
-            fig, ax = pylab.subplots(3,1)
+            wrap_frameid = int(f.hdr['START_WRITE_FRAMEID'][0])
+            ngood = 96
+            print 'WRAP FRAME', wrap_frameid, fstart, f.trigger_frameid, f.trigger_frameid - wrap_frameid, d0.shape, 'ngood', ngood
+            d0c = d0[:, chan]
+            dc = d[:, chan]
+
+            #extra = d0c[0:ncrap]
+            extra = dc[0:ngood]
+            extra = np.conj(extra[::-1])
+            c1 = scipy.signal.fftconvolve(extra, d0c)
+            c2 = scipy.signal.fftconvolve(extra, dc)
+            
+            fig, ax = pylab.subplots(4,1)
             fig.suptitle(files[0].fname + '-' +f.fname)
-            ax[0].plot(d0.real)
-            ax[0].plot(d.real)
-            ax[1].plot(d0.imag)
-            ax[1].plot(d.imag)
-            ax[2].plot((d-d0).real)
-            ax[2].plot((d-d0).imag)
+            ax[0].plot(d0c.real)
+            ax[0].plot(dc.real)
+            ax[1].plot(d0c.imag)
+            ax[1].plot(dc.imag)
+            ax[2].plot((dc-d0c).real)
+            ax[2].plot((dc-d0c).imag)
             ax[0].set_ylabel('Real part')
             ax[1].set_ylabel('Imag part')
             ax[2].set_ylabel('Difference (real/imag)')
+            ax[3].plot(abs(c1), label=files[0].fname)
+            ax[3].plot(abs(c2), label=f.fname)
+            ax[3].set_ylabel('Correlation')
+            ax[3].legend()
+
             pylab.show()
 
 
