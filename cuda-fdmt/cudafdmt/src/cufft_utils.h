9319a523e11543195d0c5c748b511de69799ba8c
Keith Bannister <keith.bannister@csiro.au>
2018-09-18 10:13:35 +0800

    Add fftbench

---
+++ cuda-fdmt/cudafdmt/src/cufft_utils.h
@@ -0,0 +1,53 @@
+
+
+/*********************/
+/* CUFFT ERROR CHECK */
+/*********************/
+static const char * _cudaGetErrorEnum(cufftResult error)
+{
+  switch (error)
+    {
+    case CUFFT_SUCCESS:
+      return "CUFFT_SUCCESS";
+
+    case CUFFT_INVALID_PLAN:
+      return "CUFFT_INVALID_PLAN";
+
+    case CUFFT_ALLOC_FAILED:
+      return "CUFFT_ALLOC_FAILED";
+
+    case CUFFT_INVALID_TYPE:
+      return "CUFFT_INVALID_TYPE";
+
+    case CUFFT_INVALID_VALUE:
+      return "CUFFT_INVALID_VALUE";
+
+    case CUFFT_INTERNAL_ERROR:
+      return "CUFFT_INTERNAL_ERROR";
+
+    case CUFFT_EXEC_FAILED:
+      return "CUFFT_EXEC_FAILED";
+
+    case CUFFT_SETUP_FAILED:
+      return "CUFFT_SETUP_FAILED";
+
+    case CUFFT_INVALID_SIZE:
+      return "CUFFT_INVALID_SIZE";
+
+    case CUFFT_UNALIGNED_DATA:
+      return "CUFFT_UNALIGNED_DATA";
+    }
+
+  return "<unknown>";
+}
+
+#define cufftSafeCall(err)      __cufftSafeCall(err, __FILE__, __LINE__)
+inline void __cufftSafeCall(cufftResult err, const char *file, const int line)
+{
+  if( CUFFT_SUCCESS != err) {
+    fprintf(stderr, "CUFFT error in file %s:%d\n %d: %s\nterminating!\n",__FILE__, __LINE__,err, \
+	    _cudaGetErrorEnum(err));
+    cudaDeviceReset();
+    assert(0);
+  }
+}
