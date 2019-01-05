d132a116c05744f66108613cad4cd0f39a4c9a9c
Chris Phillips <Chris.Phillips@csiro.au>
2019-01-05 11:10:31 +1100

    Add  rate and clip option

--- parseltongue/SN2delay.py
+++ parseltongue/SN2delay.py
@@ -3,19 +3,13 @@
 ################################################################################
 # AIPS imports
 ################################################################################
-#from AIPS import AIPS, AIPSDisk
 from AIPS import AIPS
-#from AIPSTask import AIPSTask, AIPSList
-#from AIPSTask import AIPSTask
-#from AIPSData import AIPSUVData, AIPSImage, AIPSCat
 from AIPSData import AIPSUVData
-#from AIPSTV import AIPSTV
 
 ################################################################################
 # General python imports
 ################################################################################
 import argparse, sys, os, math
-#import json
 
 ################################################################################
 # Set up AIPS stuff
@@ -31,12 +25,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--user', help="AIPS user number", type=int)
 parser.add_argument('-s', '--sn', help="SN table version", type=int)
 parser.add_argument("-a", "--av", default=False, action="store_true", help="Average IFs")
+parser.add_argument("-r", "--rate", default=False, action="store_true", help="Do rate, not delay")
+parser.add_argument("-c", "--clip", help="Ignore values with magnitude greater than this in average", type=float)
 parser.add_argument('aipsfile', help="AIPS  file ")
 args = parser.parse_args()
 
 if args.user is not None: AIPS.userno = args.user
 if args.sn is not None: snversion = args.user
 avgIf = args.av
+rate = args.rate
+clip = args.clip
+
+if clip is not None: clip /= 1e9
 
 aipsDisk = 1
 
@@ -80,33 +80,62 @@ for row in sntable:
         delaysN[ant] = 0
 
     if num_if==1:
-        rowDelay1 = [row.delay_1]
-        if num_pol>1: rowDelay2 = [row.delay_2]
-    else:
-        rowDelay1 = row.delay_1
+        if rate:
+            rowDelay1 = [row.rate_1]
+        else:
+            rowDelay1 = [row.delay_1]
         if num_pol>1:
-            rowDelay2 = row.delay_2
+            if rate:
+                rowDelay2 = [row.rate_2]
+            else:
+                rowDelay2 = [row.delay_2]
+    else:
+        if rate:
+            rowDelay1 = row.rate_1
+        else:
+            rowDelay1 = row.delay_1
+        if num_pol>1:
+            if rate:
+                rowDelay2 = row.rate_2
+            else:
+                rowDelay2 = row.delay_2
     for i in range(num_if):
         if abs(rowDelay1[i])>1 or (num_pol > 1 and abs(rowDelay2[i])>1): continue
         delays1[ant][i] += rowDelay1[i]
         if num_pol > 1: delays2[ant][i] += rowDelay2[i]
     delaysN[ant] += 1
 
+
+def strFlt(x, precision=None):
+    if precision is None:
+        return str(x)
+    else:
+        return "{:.2f}".format(x)
+
+
+p = 2
+
 if avgIf:
     for ant in delays1:
-        delaysN[ant] *= num_if
-        for i in range(1,num_if):
-            delays1[ant][0] += delays1[ant][i]
+        N = 0
+        delay = 0
+        #delaysN[ant] *= num_if
+        for i in range(num_if):
+            if clip is None or abs(delays1[ant][i])<clip:
+                delay += delays1[ant][i]
+                N += 1
         if num_pol>1: 
-            delaysN[ant] *= 2
             for i in range(num_if):
-                delays1[ant][0] += delays2[ant][i]
-
-    for ant in sorted(delays1):
-        if delaysN[ant]==0:
-            print ant, 0
+                if clip is None or abs(delays2[ant][i])<clip:
+                    delay += delays2[ant][i]
+                    N += 1
+        if N == 0:
+            delays1[ant][0] = 0
         else:
-            print ant, delays1[ant][0]/delaysN[ant]*1e9
+            delays1[ant][0] = delay/N*1e9
+            
+    for ant in sorted(delays1):
+        print ant, delays1[ant][0]
 
 else:
 
@@ -115,6 +144,6 @@ else:
             print ant, 0
         else:
             if num_pol>1: 
-                print ant, ','.join([str(delays1[ant][i]/delaysN[ant]*1e9)+','+str(delays2[ant][i]/delaysN[ant]*1e9) for i in range(num_if)])
+                print ant, ','.join([strFlt(delays1[ant][i]/delaysN[ant]*1e9,p)+','+strFlt(delays2[ant][i]/delaysN[ant]*1e9,p) for i in range(num_if)])
             else:
                 print ant, ','.join([str(delays1[ant][i]/delaysN[ant]*1e9) for i in range(num_if)])
