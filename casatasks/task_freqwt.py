913af747656f33dc1c5cd82a313d79b8f7578304
Bannister, Keith (CASS, Marsfield) <Keith.Bannister@csiro.au>
2019-02-11 15:42:19 +1100

    Minor improvements to freqwt casa task

--- casatasks/task_freqwt.py
+++ casatasks/task_freqwt.py
@@ -1,21 +1,75 @@
 from taskinit import *
 import numpy as np
 from scipy.interpolate import interp1d
+import re
+
+
+def load_spectrum(fname, polre=r'.*TARGET\.\cube\.(.*)\.image-raster'):
+    '''
+    Loads spectrum from a text file - produced by CASA (somehow?)
+    
+    Assumes a file with lines starting with # as comments
+    Each non-comment line contains freq in GHz and weight value separated by white space.
+    Polarisation-dependant weights can be included in the file.
+    Each polarisation should be prepended by a comment that satisfies the polarisation regexp. The first matched group of the regexp is used as the key for the return dictionaryy.
+    If no polarisation header is in the file, 'I' is assumed
+    :returns: A dictionary, keyed by polarisation type, a 2-D numpy array of (freqGHz, value)
+    '''
+    polre = re.compile(polre)
+    currpol = 'I'
+    out = {}
+    out[currpol] = []
+    with open(fname, 'rU') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('#'): # its a comment
+                m = polre.match(line)
+                if m is None: # no match - boring commment - continue to next line
+                    continue
+                else: # it's a new polarisation
+                    currpol = m.groups(1)[0]
+                    out[currpol] = []
+            else:
+                bits = line.split()
+                if len(bits) == 2:
+                    out[currpol].append(map(float, bits))
+
+    # convert to numpy array
+    out2 = {}
+    for k, v in out.iteritems():
+        out2[k] = np.array(v)
+        assert out2[k].ndim == 2
+
+    return out2
+    
+def plot_spec(spec):
+    '''
+    Plot the spectra loaded from load_spec
+    '''
+    from pylab import subplots, draw
+    fig, ax = subplots(1,1)
+    for pol, specd in spec.iteritems():
+        f = specd[:, 0]
+        a = specd[:, 1]
+        ax.plot(f,a, label=pol)
+
+    ax.set_xlabel('Frequency (GHz)')
+    ax.legend()
+    draw()
 
 def freqwt(vis, specfile, weightdata=True, cutoff=0.0):
 
     casalog.origin('freqwt')
-    weights = np.loadtxt(specfile)
-    if weights.ndim != 2 or weights.shape[1] != 2:
-        casalog.post('Invalid weights shape {}'.format(weights.shape), priority='ERROR')
-        return
+    spec = load_spectrum(specfile)
+    weight_interp = {}
+    for pol, weightd in weights.iteritems():
+        weightsf = weightd[:, 0]
+        weightsa = weightd[:, 1]
+        casalog.post('Weights file {} pol {} has {} frequencies from {:02f}-{:02f} GHz amplitude max/min/mean/std = {:02f}/{:02f}/{:02f}/{:02f}'
+                     .format(specfile, pol, weightd.shape[0], weightsf[0], weightsf[1], weightsa.max(), weightsa.min(), \
+                             weightsa.mean(), weightsa.std()))
+        weight_interp[pol] = interp1d(weightsf, weightsa, kind='linear', fill_value='extrapolate')
 
-    
-    weightsf = weights[:, 0]
-    weightsa = weights[:, 1]
-
-    casalog.post('Weights from {}-{} GHz amplitude max/min/mean/std = {}/{}/{}/{}'.format(weightsf[0], weightsf[1], weightsa.max(), weightsa.min(), weightsa.mean(), weightsa.std()))
-    interp = interp1d(weightsf, weightsa, kind='linear', fill_value='extrapolate')
     ms.open(vis)
     specwin = ms.getspectralwindowinfo()
     ms.close()
@@ -24,11 +78,11 @@ def freqwt(vis, specfile, weightdata=True, cutoff=0.0):
         return
 
     for w in specwin.values():
-        chan_ghz = np.arange(w['NumChan'])*w['ChanWidth'] + w['Chan1Freq']
-        chan_ghz /= 1e9
+        chan_ghz = (np.arange(w['NumChan'])*w['ChanWidth'] + w['Chan1Freq'])/1e9
         w['ChanGhz'] = chan_ghz
 
         # interpolate onto data grid
+        interp = weight_interp['I'] # only support stokes I for now
         wamp = interp(chan_ghz)
 
         # set values less than threshold to 0
@@ -43,7 +97,7 @@ def freqwt(vis, specfile, weightdata=True, cutoff=0.0):
         w['weightspec'] = weightspec
         w['wamp'] = wamp
         w['wmask'] = wmask
-        casalog.post('Got {} of {} valid weights above the cutoff'.format(sum(w['wmask']), len(wamp)))
+        casalog.post('Got {} of {} valid weights above the cutoff of {}'.format(sum(w['wmask']), len(wamp), cutoff))
 
 
     tb.open(vis, nomodify=False)
@@ -59,6 +113,7 @@ def freqwt(vis, specfile, weightdata=True, cutoff=0.0):
 
         if weightdata:
             ind = tb.getcell('DATA', r)
+            # ind shape is (pol, freq)
             ind[:, wmask] /= wamp[wmask]
             ind[:, ~wmask] = 0
             tb.putcell('DATA', r, ind)
