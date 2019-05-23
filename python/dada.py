Fix up masking for nans in kurtosis

python/dada.py
@@ -39,7 +39,7 @@ class DadaFile(object):
             raise ValueError('Invalid blockid {}'.format(blockid))
         
         if blockid >= self.nblocks:
-            raise ValueError('BlockID {} past the end of file'.format(blockid))
+            raise ValueError('BlockID {} past the end of file {}'.format(blockid, self.filename))
 
         assert 0 <= blockid < self.nblocks
 
@@ -54,7 +54,7 @@ class DadaFile(object):
             'max at', \
             np.unravel_index(v.argmax(), v.shape), 'NaNs?', np.sum(np.isnan(v))
 
-        return v
+        return np.ma.masked_invalid(v)
 
     def blocks(self, step=1):
         '''

python/plotfreddars.py
@@ -13,7 +13,6 @@ import sys
 import logging
 from rtdata import FreddaRescaleData
 
-
 __author__ = "Keith Bannister <keith.bannister@csiro.au>"
 
 class Formatter(object):
@@ -138,8 +137,11 @@ class RescalePlot(object):
             if name == 'gtest':
                 tsamp = 2047.
                 bdn = bd['mean']**2/bd['std']**2 / tsamp - 1.0
+                if prevbd:
+                    prevbd['gtest'] = prevbd['mean']**2/prevbd['std']**2 / tsamp - 1.0
             else:
                 bdn = bd[name]
+                
             if values.log:
                 bdn= 10*np.log10(bdn)
             elif values.lognz: