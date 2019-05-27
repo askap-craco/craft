52ead6a1ddb69b078774febcd94089a75f9d72f9
Keith Bannister <keith.bannister@csiro.au>
2019-05-27 16:11:53 +0800

    Add filclean.py CRAFT-295

--- python/filclean.py
+++ python/filclean.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """
-Plots a lot of beams
+Cleans multibeam filterbanks using eigenflagging/subtraction.
 
-Copyright (C) CSIRO 2015
+See Kocz 2012 and Wax and Kailath 1985
+
+Copyright (C) CSIRO 2018
 """
 import pylab
 import matplotlib as mpl
@@ -11,139 +13,53 @@ import numpy as np
 import os
 import sys
 import logging
-import itertools
-from craftobs import load_beams
-from plotutil import subplots
-import matplotlib.gridspec as gridspec
 import sigproc
 
 __author__ = "Keith Bannister <keith.bannister@csiro.au>"
 
-def onpick(event):
-    thisline = event.artist
-    xdata, ydata = thisline.get_data()
-    ind = event.ind
-
-    print thisline.get_label(), xdata[ind], ydata[ind]
-
-def annotate(fig, title, xlabel, ylabel):
-    fig.text( 0.5, 0.98,title, ha='center', va='top')
-    fig.text(0.5, 0.02, xlabel, ha='center', va='bottom')
-    fig.text(0.02, 0.5, ylabel, rotation=90, ha='center', va='top')
-
-def commasep(s):
-    return map(int, s.split(','))
-
-def floatcommasep(s):
-    return map(float, s.split(','))
-
-def next_divisor(x, n):
-    for t in xrange(x, n/2):
-        if n % t == 0:
-            return t
-
-def divisors(n):
-    d = [t for t in xrange(1, n/2 + 1) if n % t == 0]
-    return d
-
 def _main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(description='Script description', formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
-    parser.add_argument('-o','--outfile', help='Output file')
-    parser.add_argument('-A', '--suffix', help='OUtput file suffix')
-    parser.add_argument('-s', '--show',  action='store_true', help='show plots')
-
+    parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+    parser.add_argument('-n','--nsamp', type=int, help='Number of samples per block to calculate covariance over and subtract', default=256)
+    parser.add_argument('-k', '--nsub', type=int, default=1, help='Fixed Number of eigenvalues to subtract (todo: Dynamic)')
+    parser.add_argument('-D','--outdir', required=True, help='Output directory for cleaned filterbanks')
     parser.add_argument(dest='files', nargs='+')
-    parser.set_defaults(verbose=False, nxy="1,1")
+    parser.set_defaults(verbose=False)
     values = parser.parse_args()
     if values.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    for f in values.files:
-        filclean(f, values)
+    insfs = [sigproc.SigprocFile(f) for f in values.files]
+    outfile_names = [os.path.join(values.outdir, os.path.basename(f)) for f in values.files]
+    outsfs = [sigproc.SigprocFile(f, mode='w', header=s.header) for f, s in zip(outfile_names, insfs)]
+    endsamp = min([s.nsamples for s in insfs])
+    nsamp = values.nsamp
+    nblock = endsamp // nsamp
+    k = values.nsub
 
-def filclean(fname, values):
-    f = sigproc.SigprocFile(fname)
-    tstart = 0
-    ntimes = 4096
-    tend = tstart + ntimes
-    nelements = ntimes*f.nifs*f.nchans
-    assert f.nifs == 1
-    #f.seek_data(f.bytes_per_element*tstart)
-    spout = None
-    outf = None
-    if values.outfile:
-        outf = values.outfile
-    elif values.suffix:
-        outf = fname + '.' + values.suffix
-        
-    if outf:
-        spout = sigproc.SigprocFile(outf, 'w', header=f.header)
+    for b in xrange(nblock):
+        d = np.array([s[b*nsamp:(b+1)*nsamp] for s in insfs])
+        nbeam, nint, nchan = d.shape
+        dout = np.empty_like(d)
+        for ic in xrange(nchan):
+            r = d[:, :, ic]
+            thecov = np.cov(r)
+            #dcovar[ic, :, :] = thecov
+            u, v, vh = np.linalg.svd(thecov)
+            for t in xrange(nint):
+                rfi = np.dot(u.T, r[:, t])
+                #rfiout[:, t, ic] = rfi[:]
+                rfi[:k] = 0
+                dout[:, t, ic] = np.dot(rfi, u.T)
+                
+        for iout, sout in enumerate(outsfs):
+            dout[iout, :, :].astype(d.dtype).flatten().tofile(sout.fin)
 
-    while True:
-        dtype = np.uint8
-        v = np.fromfile(f.fin, dtype=dtype, count=nelements )
-        if len(v) != ntimes*f.nchans:
-            # finished
-            break
-        v.shape = (ntimes, f.nchans)
-        #v -= 128.
-        #v /= 18.
-
-        dm0 = v.mean(axis=1)
-        offset = dm0.mean()
-        # shape = time, channel
-        vs = v - dm0[:, None] + offset # broadcast DM0
-        print 'SHapes', v.shape, dm0[:, None].shape, vs.dtype
-
-
-        if spout:
-            vs.astype(np.uint8).flatten().tofile(spout.fin)
-
-        dm0_vs = vs.mean(axis=1) 
-
-        if values.show:
-            
-            f, (ax1, ax2) = pylab.subplots(1,2)
-            ax1.imshow(v.T, aspect='auto')
-            ax2.imshow(vs.T, aspect='auto')
-
-            #bins = np.arange(-6, 6, 0.01)
-            bins =100
-            pylab.figure()
-
-            pylab.hist(v.flatten(), histtype='step', bins=bins)
-            pylab.hist(vs.flatten(), histtype='step', bins=bins)
-
-            f, (ax1, ax2) = pylab.subplots(1,2)
-            vf = abs(np.fft.rfft(v, axis=0))**2
-            vsf = abs(np.fft.rfft(vs, axis=0))**2
-            ax1.imshow(np.log10(vf[1:, 1:].T), aspect='auto')
-            ax2.imshow(np.log10(vsf[1:, 1:].T), aspect='auto')
-            
-
-            pylab.figure()
-            pylab.plot(v.std(axis=0))
-            pylab.plot(vs.std(axis=0))
-    
-            pylab.figure()
-            pylab.plot(dm0)
-            pylab.plot(dm0_vs)
-            pylab.figure()
-            pylab.plot(abs(np.fft.rfft(dm0))**2)
-            pylab.plot(abs(np.fft.rfft(dm0_vs))**2)
-            
-            
-            pylab.show()
-
-    if spout:
-        spout.fin.flush()
-        spout.fin.close()
-    
-
+    for s in outsfs:
+        s.close()
 
 if __name__ == '__main__':
     _main()
