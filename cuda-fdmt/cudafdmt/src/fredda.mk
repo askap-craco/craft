3703ee59ca8c5ee976c1846b91eab3bba33dfb0d
XinpingDeng <xinping.deng@csiro.au>
2019-08-12 10:19:30 +1000

    attempt

--- cuda-fdmt/cudafdmt/src/fredda.mk
+++ cuda-fdmt/cudafdmt/src/fredda.mk
@@ -1,12 +1,16 @@
 # site-specific includes
 # path to cuda - shoudl contain bin/nvcc
 CUDA_PATH=/usr/
+
 # target cuda architecture e.g. compute_60
-CUDA_ARCH=compute_60
+CUDA_ARCH=compute_70
+
 # target cuda code = e.g. sm_60
-CUDA_CODE=sm_60
+CUDA_CODE=sm_70
+
 # path to cub.h from cub-1.6.4 https://nvlabs.github.io/cub/
-CUB_PATH=/u/ban115/build/cub/cub-1.6.4/cub/
+CUB_PATH=$(KEITH_HOME)/build/cub/cub-1.6.4/cub/
+
 # path to PSRDADA: http://psrdada.sourceforge.net/
 #DADA_PATH=/home/craftop/askap/trunk/3rdParty/psrdada/psrdada-537159/install/
-DADA_PATH=/u/ban115/psrdada-install/
+DADA_PATH=$(KEITH_HOME)/psrdada-install/
